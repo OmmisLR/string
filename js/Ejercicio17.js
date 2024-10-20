350e2a3f8908d3cdd2016627e3e2701f44097b9e
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:54:45 -0600

    Update Ejercicio17.js

--- js/Ejercicio17.js
+++ js/Ejercicio17.js
@@ -1,7 +1,3 @@
-/*Crea una función que cuente cuántas palabras hay en una cadena usando split() y
-length.
-Ejemplo: "Aprender JavaScript es divertido" → 4 */
-
 function contar() {
     var cadena = prompt("Introduce una cadena de texto:");
     var palabras = cadena.split(" ");
