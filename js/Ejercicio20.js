d97307955781298416b017ae25dc529fd00b2436
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:55:31 -0600

    Update Ejercicio20.js

--- js/Ejercicio20.js
+++ js/Ejercicio20.js
@@ -1,7 +1,3 @@
-/*Escribe una función que verifique si una cadena termina con una palabra específica
-usando endsWith().
-Ejemplo: "JavaScript es genial", "genial" → true */
-
 function verificar() {
     var cadena = prompt("Introduce una cadena de texto:");
     var palabraFinal = prompt("La palabra con la que termina es:");
