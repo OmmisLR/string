f48cc897b95fc2c4fba2b894dfab7bb33925b724
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:52:59 -0600

    Update Ejercicio10.js

--- js/Ejercicio10.js
+++ js/Ejercicio10.js
@@ -1,7 +1,3 @@
-/*Escribe una función que cuente cuántas veces aparece una letra específica en una
-cadena.
-Ejemplo: "javascript", "a" → 2 */
-
 function contarLetra(cadena, letra) {
     let contador = 0;
     for (let i = 0; i < cadena.length; i++) {
