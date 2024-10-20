8b51d096988af5dfcef518a2f1d1cf774c82998c
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:55:09 -0600

    Update Ejercicio19.js

--- js/Ejercicio19.js
+++ js/Ejercicio19.js
@@ -1,7 +1,3 @@
-/*Crea una función que verifique si una cadena comienza con una palabra específica
-usando startsWith().
-Ejemplo: "JavaScript es genial", "JavaScript" → true */
-
 function verificar() {
     var cadena = prompt("Introduce una cadena de texto:");
     var palabraInicio = prompt("La palabra con la que debe iniciar es:");
