040c9102cca05e96a961f44734e5550e799e6a20
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:53:13 -0600

    Update Ejercicio11.js

--- js/Ejercicio11.js
+++ js/Ejercicio11.js
@@ -1,7 +1,3 @@
-/*Crea una función que tome una cadena y la devuelva invertida.
-Ejemplo: "Hola" → "aloH" */
-
-
 function invertirCadena() {
     const cadena = prompt("Por favor, introduce una cadena:");
     const cadenaInvertida = cadena.split('').reverse().join('');
