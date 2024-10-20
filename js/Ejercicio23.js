0cf2dd1350e246bb1f0edcee887e4523685dbe9d
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:56:03 -0600

    Update Ejercicio23.js

--- js/Ejercicio23.js
+++ js/Ejercicio23.js
@@ -1,7 +1,3 @@
-/*Crea una función que trunque una cadena a un número específico de caracteres,
-agregando ... al final si es más larga.
-Ejemplo: "JavaScript es genial", 10 → "JavaScri..." */
-
 function truncarCadena() {
     var cadena = prompt("Introduce una cadena de texto:");
     var longitudMaxima = parseInt(prompt("¿Cuántos caracteres quieres que tenga?"));
