2ddcc40f61ca2609bd8ffde97fdfe90719a95fce
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:54:18 -0600

    Update Ejercicio15.js

--- js/Ejercicio15.js
+++ js/Ejercicio15.js
@@ -1,6 +1,3 @@
-/*Crea una función que devuelva el último carácter de una cadena sin usar length.
-Ejemplo: "Hola" → "a" */
-
 const obtenerUltimoCaracter = (cadena) => {
     let ultimoCaracter = ""; 
     for (let i = 0; ; i++) {
