824e552d6b92a78a8d5b7a0718f2380063cd8cc0
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:58:02 -0600

    Update Ejercicio4.js

--- js/Ejercicio4.js
+++ js/Ejercicio4.js
@@ -1,7 +1,3 @@
-/*Crea una función que verifique si una palabra específica está contenida dentro de
-una cadena usando includes().
-Ejemplo: "JavaScript es genial", "genial" → true */
-
 function verificar(cadena, palabra) {
     return cadena.includes(palabra);
 }
