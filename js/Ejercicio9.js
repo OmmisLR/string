3d85b97a27b69dfd378e737b4698c53930268500
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:59:03 -0600

    Update Ejercicio9.js

--- js/Ejercicio9.js
+++ js/Ejercicio9.js
@@ -1,5 +1,3 @@
-/*Crea una función que convierta la primera letra de una cadena en mayúscula.
-Ejemplo: "javascript" → "Javascript" */
 function capitalizarPrimeraLetra(cadena) {
     if (cadena.length === 0) return ""; 
     return cadena.charAt(0).toUpperCase() + cadena.slice(1);
