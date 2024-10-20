52560c47a5c7bc66a659b90fca9e703cc6ed8442
OmmisLR <omartorres10884@gmail.com>
2024-10-19 20:55:41 -0600

    Update Ejercicio21.js

--- js/Ejercicio21.js
+++ js/Ejercicio21.js
@@ -1,7 +1,3 @@
-/*Crea una función que reemplace todas las ocurrencias de una palabra en una
-cadena usando replaceAll().
-Ejemplo: "hola hola", "hola", "adiós" → "adiós adiós" */
-
 function reemplazarPalabra() {
     var cadena = prompt("Introduce una cadena de texto:");
     var palabraOriginal = prompt("¿Qué palabra quieres reemplazar?");
